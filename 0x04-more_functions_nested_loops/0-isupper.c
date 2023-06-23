6342dbe9b12efbe14a284f80ce77be8fcc4a5f61
Avhukie98-12 <avhu.prudy@gmail.com>
2023-06-23 07:06:48 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _1supper - check if a letter is supper
+ * @x: The number to be checked
+ * Return: 1 for supper letter or 0 for any else
+ */
+ 
+_1supper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
